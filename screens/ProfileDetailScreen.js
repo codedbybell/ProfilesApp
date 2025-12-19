817d661228cca483735335c0814f419833f5c056
uniqueunp <120202013+uniqueunp@users.noreply.github.com>
2025-12-19 21:59:43 +0300

    Implement navigation with Profiles and ProfileDetail screens, and update dependencies

---
+++ screens/ProfileDetailScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function ProfileDetailScreen() {
+    return <View><Text>Detail Screen</Text></View>;
+}
---
+++ screens/ProfilesListScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function ProfilesListScreen() {
+    return <View><Text>List Screen</Text></View>;
+}
